195cfc754d11739a84d9195987d02125715ffec6
Stian Tvedt <stian@Macbook.local>
2020-11-03 17:07:03 +0100

    added fire_app

--- firewatch_project/firewatch_project/urls.py
+++ firewatch_project/firewatch_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fire_app import views
 
 urlpatterns = [
+    path('', views.home, name='FireApp'),
     path('admin/', admin.site.urls),
 ]
